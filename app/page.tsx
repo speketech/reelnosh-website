4e22064703f44a2665b6966dd424ff7e8b05f767
Qudrot <ibeabuchikudirat@gmail.com>
2026-09-02 11:50:13 +0100

    chore: clean slate for redesign

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Hero } from "@/components/sections/Hero";
-
-export default function Home() {
-  return (
-    <main className="flex-1 w-full bg-warmWhite">
-      <Hero />
-    </main>
-  );
-}
